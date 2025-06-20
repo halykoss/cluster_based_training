7fb89e895d47d44e5699e44e3c3125332e14c69d
Federico Pennino <federico.p98@gmail.com>
2025-06-20 19:04:07 +0200

    sampling fix

--- dataset/EncodedDataset.py
+++ dataset/EncodedDataset.py
@@ -114,7 +114,7 @@ class EncodedDataset(Dataset):
         for cluster_id in range(36):
             print("Dim cluster_labels:", self.cluster_labels.shape)
             cluster_samples = np.sum(self.cluster_labels == cluster_id)
-            samples_to_take = cluster_samples * weights[cluster_id]
+            samples_to_take = int(cluster_samples * weights[cluster_id])
 
             selected_indices = np.random.choice(
                 cluster_samples, size=samples_to_take, replace=False
