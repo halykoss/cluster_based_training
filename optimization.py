68f7826f41b9444816ca8b7c52c7f6510e60d924
Federico Pennino <federico.p98@gmail.com>
2025-06-20 17:45:14 +0200

    weights_dir fixed

--- optimization.py
+++ optimization.py
@@ -37,7 +37,8 @@ def optimize(trial):
     
     cluster_weights = [trial.suggest_float(f"weight_{i}", 0.0, 1.0) for i in range(36)]
     weights_dir = "data/weights/"
-    os.makedirs(weights_dir)
+    if not os.path.exists(weights_dir):
+        os.makedirs(weights_dir)
     np.save(os.path.join(weights_dir, "cluster_weights_trial_{}.npy".format(trial.number)), train_dataset.sampled_indices)
     
     sys.stderr.write(f"Trial {trial.number}, campionamento dei pesi dei cluster:\n")
